129bc96d36b896e9f5cbadf0a0e05771316cd5de
catalice-prod <catalicedesign@gmail.com>
2025-04-05 22:55:22 +0200

    Title Changed

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ function App() {
 
   return (
     <main >
-      <h1>My todos</h1>
+      <h1>Welcome!</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (

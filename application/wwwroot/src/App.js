90e36098c503c3ed90b8f187837974ceb4b70c64
Gianclaudio Moura <Gianclaudio.moura@sage.com>
2025-04-16 21:49:40 +0200

    add robo trust

--- application/wwwroot/src/App.js
+++ application/wwwroot/src/App.js
@@ -1,22 +1,18 @@
-import logo from './logo.svg';
+import logo from './robo.jpeg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>
+          JobScout
+        </h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Trust in robots.
+          Don't trust in Headhunters.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className="App-logo" alt="logo" />
       </header>
     </div>
   );

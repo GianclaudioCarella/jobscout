342aa4eabf0844d205349ef22641b5373f6a57b0
gmccc <gianclaudiocarella@hotmail.com>
2025-06-27 18:47:04 +0200

    web c# added

---
+++ web/jobscout-web.Server/Controllers/JobsController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using SerpApi;
+using System.Collections;
+using System.Text;
+
+namespace jobscout_web.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class JobsController : ControllerBase
+    {       
+        private readonly ILogger<JobsController> _logger;
+
+        public JobsController(ILogger<JobsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetJobs")]
+        public IEnumerable<JobScoutResult> Get(string jobTitle, string location, string companies)
+        {
+            String apiKey = "";
+
+            Hashtable ht = new Hashtable();
+            ht.Add("q", "Jobs in " + location + " for " + jobTitle + "in these companies: " + companies);
+            ht.Add("location", location);
+            ht.Add("hl", "en");
+            ht.Add("gl", "us");
+            ht.Add("google_domain", "google.com");
+
+            StringBuilder stringBuilder = new StringBuilder();
+            List<JobScoutResult> jobScoutResultsList = new List<JobScoutResult>();
+
+            try
+            {
+                GoogleSearch search = new GoogleSearch(ht, apiKey);
+                JObject data = search.GetJson();
+                JArray results = (JArray)data["organic_results"];
+
+                foreach (JObject result in results)
+                {
+                    JobScoutResult jobScoutResult = new JobScoutResult() { Title = result["title"].ToString(), Url = result["link"].ToString() };
+                    jobScoutResultsList.Add(jobScoutResult);
+                }
+            }
+            catch (SerpApiSearchException ex)
+            {
+                Console.WriteLine("Exception:");
+                Console.WriteLine(ex.ToString());
+            }
+
+            return jobScoutResultsList;
+        }
+    }
+
+    public class JobScoutResult
+    {
+        public string Title { get; set; }
+        public string Url { get; set; }
+    }
+}
---
+++ web/jobscout-web.client/src/App.tsx
@@ -0,0 +1,96 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+
+interface JobscoutResult {
+    title: string;
+    url: string;
+}
+
+function App() {
+    const [jobscoutResults, setJobscoutResults] = useState<JobscoutResult[]>();
+
+    const [jobTitle, setJobTitle] = useState('');
+    const [location, setLocation] = useState('');
+    const [companies, setCompanies] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
+
+    useEffect(() => {
+        
+    }, []);
+
+    function handleButtonClick() {
+        setIsLoading(true);
+         populateJobscoutData();
+        setIsLoading(false);
+        //useEffect();
+    }
+
+    const contents = isLoading
+        ? <p><em>Loading...</em></p>
+        : jobscoutResults === undefined
+            ? <p><em>Waiting</em></p>
+            : <table className="table table-striped" aria-labelledby="tableLabel">
+                <thead>
+                    <tr>
+                        <th>Title</th>
+                        <th>Url</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {jobscoutResults.map(result =>
+                        <tr key={result.title}>
+                            <td>{result.title}</td>
+                            <td>
+                                <a href={result.url} target="_blank" rel="noopener noreferrer">
+                                    {result.url}
+                                </a>
+                            </td>
+                        </tr>
+                    )}
+                </tbody>
+            </table>;
+
+    return (
+        <div>
+            <h1 id="tableLabel">Welcome to Jobscout</h1>
+            <p>Tool to help you to find your next job.</p>
+            <div>
+                <input
+                    type="text"
+                    placeholder="Job Title"
+                    value={jobTitle}
+                    onChange={e => setJobTitle(e.target.value)}
+                />
+                <input
+                    type="text"
+                    placeholder="Location"
+                    value={location}
+                    onChange={e => setLocation(e.target.value)}
+                />
+                <input  
+                    type="text"
+                    placeholder="Companies"
+                    value={companies}
+                    onChange={e => setCompanies(e.target.value)}
+                />
+            </div>
+            <button onClick={handleButtonClick}>Search</button>
+            {contents}
+        </div>
+    );
+
+    async function populateJobscoutData() {
+        const params = new URLSearchParams({
+            jobTitle,
+            location,
+            companies
+        });
+        const response = await fetch(`jobs?${params.toString()}`);
+        if (response.ok) {
+            const data = await response.json();
+            setJobscoutResults(data);
+        }
+    }
+}
+
+export default App;
